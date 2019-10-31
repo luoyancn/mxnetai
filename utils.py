835a3ba92c4a747ccbb53ba6f9d2f55a9a7faaaa
luoyancn <luoyan_cn@outlook.com>
2019-10-31 15:04:37 +0800

    Add gpu usage for gluon

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from mxnet import ndarray as nd
 from mxnet import gluon
 from mxnet import image
 from mxnet import autograd
+from mxnet.gluon import nn
 
 import numpy as np
 
@@ -171,3 +172,45 @@ def _get_batch(batch, ctx):
     else:
         data, label = batch
     return data.as_in_context(ctx), label.as_in_context(ctx)
+
+
+class Residual(nn.HybridBlock):
+    def __init__(self, channels, same_shape=True, **kwargs):
+        super(Residual, self).__init__(**kwargs)
+        self.same_shape = same_shape
+        with self.name_scope():
+            strides = 1 if same_shape else 2
+            self.conv1 = nn.Conv2D(channels, kernel_size=3, padding=1,
+                                  strides=strides)
+            self.bn1 = nn.BatchNorm()
+            self.conv2 = nn.Conv2D(channels, kernel_size=3, padding=1)
+            self.bn2 = nn.BatchNorm()
+            if not same_shape:
+                self.conv3 = nn.Conv2D(channels, kernel_size=1,
+                                      strides=strides)
+
+    def hybrid_forward(self, F, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if not self.same_shape:
+            x = self.conv3(x)
+        return F.relu(out + x)
+
+
+def resnet18_28(num_classes):
+    net = nn.HybridSequential()
+    with net.name_scope():
+        net.add(
+            nn.BatchNorm(),
+            nn.Conv2D(64, kernel_size=3, strides=1),
+            nn.MaxPool2D(pool_size=3, strides=2),
+            Residual(64),
+            Residual(64),
+            Residual(128, same_shape=False),
+            Residual(128),
+            Residual(256, same_shape=False),
+            Residual(256),
+            nn.AvgPool2D(pool_size=3),
+            nn.Dense(num_classes)
+        )
+    return net
